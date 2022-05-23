a80531837c3ee0e0fc712a19217f1639fa3f09f1
Justin Taylor Weiss <justintaylorweiss@gmail.com>
2022-05-23 00:28:21 -0400

    added log to info panel and fixed styling

--- src/components/infoPanel/InfoPanelSubcomponents.js
+++ src/components/infoPanel/InfoPanelSubcomponents.js
@@ -1,5 +1,7 @@
+import styled from 'styled-components';
 import { ProgressBar } from '../progressBar/ProgressBar';
 import { TitleText } from '../titleText/TitleText';
+import { TextLog } from '../textLog/TextLog';
 
 export const PlayerName = ({}) => (
     <TitleText 
@@ -7,24 +9,41 @@ export const PlayerName = ({}) => (
         text="Player"
     />
 )
-export const HealthBar = ({}) => (
+const UnfHealthBar = ({className}) => (
     <ProgressBar
+        className={className}
         gridArea="Health"
         currentValue={75} 
         maxValue={100} 
-        height="55%" 
+        height="70%" 
         backgroundColor="#FF0000"
         barColor="#00FF00"
     />
 )
 
-export const XPBar = ({}) => (
+export const HealthBar = styled(UnfHealthBar)`
+    padding: 0px 20px;
+`;
+
+const UnfXPBar = ({className}) => (
     <ProgressBar
+        className={className}
         gridArea="XP"
         currentValue={20} 
         maxValue={100} 
         height="50%" 
         backgroundColor="#FFFFFF"
         barColor="#0000FF"
+    />
+)
+
+export const XPBar = styled(UnfXPBar)`
+    padding: 0px 20px;
+`;
+
+export const Log = ({log}) => (
+    <TextLog
+    gridArea="Log"
+    log={log}
     />
 )
